f742570e495deabe5c4a7488fdf9bfb9113eec5c
mpetitfrere <rentenkz@gmail.com>
2016-11-28 10:18:30 -0500

    added asynctask

--- mobile/src/main/java/com/example/healthmonitoring/RecyclerAdapterDoctor.java
+++ mobile/src/main/java/com/example/healthmonitoring/RecyclerAdapterDoctor.java
@@ -2,9 +2,10 @@ package com.example.healthmonitoring;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
 import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,12 @@ import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+
+import static com.google.android.gms.plus.PlusOneDummyView.TAG;
 
 /**
  * Created by rtrev on 10/23/2016.
@@ -21,6 +27,9 @@ import java.util.List;
 public class RecyclerAdapterDoctor extends RecyclerView.Adapter<RecyclerAdapterDoctor.ViewHolder> {
 
     Context context;
+    String threshold;
+    String patientId;
+    private BackgroundTask task;
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
@@ -86,11 +95,15 @@ public class RecyclerAdapterDoctor extends RecyclerView.Adapter<RecyclerAdapterD
                     @Override
                     public void onClick(View v) {
                         m_Text[0] = String.valueOf(np.getValue());
+                        threshold = m_Text[0];;
+                        patientId = patientDoctors.get(position).patientID;
+                        task = new BackgroundTask(context);
+                        task.execute();
 
-                        Intent i = new Intent(view.getContext(), ChangeThreshold.class);
-                        i.putExtra("patientThreshold", m_Text[0]);
-                        i.putExtra("patientId", patientDoctors.get(position).patientID);
-                        view.getContext().startActivity(i);
+//                        Intent i = new Intent(view.getContext(), ChangeThreshold.class);
+//                        i.putExtra("patientThreshold", m_Text[0]);
+//                        i.putExtra("patientId", patientDoctors.get(position).patientID);
+//                        view.getContext().startActivity(i);
                         dialog.dismiss();
 
                     }
@@ -113,6 +126,53 @@ public class RecyclerAdapterDoctor extends RecyclerView.Adapter<RecyclerAdapterD
     @Override
     public int getItemCount() {
         return patientDoctors.size();
+    }
+    class BackgroundTask extends AsyncTask<String, Void, Boolean> {
+
+        private Context context;
+
+        public BackgroundTask(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+
+            try {
+
+                Connection conn = SQLConnection.doInBackground();
+
+                String sql = "update healthApp.Patient set HR_Limits = " + threshold + " where Id = " + patientId;
+
+                PreparedStatement prepare = conn.prepareStatement(sql);
+
+                prepare.executeUpdate();
+                prepare.close();
+
+                return true;
+
+
+            } catch (SQLException e) {
+                Log.d(TAG, e.getMessage());
+            }
+
+            Log.d(TAG, "asynctask ouside false");
+            return true;
+
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            if (result) {
+                Log.d(TAG, "Good Job");
+                //Toast.makeText(context, "Update Threshold to " + threshold + " for patient " + patientId, Toast.LENGTH_SHORT).show();
+                notifyDataSetChanged();
+                //retrieveMessage("button");
+            } else
+                Log.d(TAG, "Good Effort");
+        }
+
     }
 
 }
