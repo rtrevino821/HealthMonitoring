9ba95bfec265a1c9bc1f2cbcf61bc237e931392d
mpetitfrere <rentenkz@gmail.com>
2016-11-28 10:06:21 -0500

    Asynctask in RAD

--- mobile/src/main/java/com/example/healthmonitoring/RecyclerAdapterDoctor.java
+++ mobile/src/main/java/com/example/healthmonitoring/RecyclerAdapterDoctor.java
@@ -2,7 +2,7 @@ package com.example.healthmonitoring;
 
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.text.InputType;
@@ -13,7 +13,12 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+
+import static com.google.android.gms.plus.PlusOneDummyView.TAG;
 
 /**
  * Created by rtrev on 10/23/2016.
@@ -21,12 +26,16 @@ import java.util.List;
 public class RecyclerAdapterDoctor extends RecyclerView.Adapter<RecyclerAdapterDoctor.ViewHolder> {
 
     Context context;
+    String threshold;
+    String patientId;
+    private BackgroundTask task;
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
         public TextView itemPatientName;
         public TextView itemLastVisit;
         public TextView itemThreshold;
+
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -80,10 +89,16 @@ public class RecyclerAdapterDoctor extends RecyclerView.Adapter<RecyclerAdapterD
                             m_Text[0] = input.getText().toString();
                             Log.d("TAG", m_Text[0]);
 
-                            Intent i = new Intent(view.getContext(), ChangeThreshold.class);
-                            i.putExtra("patientThreshold", m_Text[0]);
-                            i.putExtra("patientId", patientDoctors.get(position).patientID);
-                            view.getContext().startActivity(i);
+                            threshold = m_Text[0];;
+                            patientId = patientDoctors.get(position).patientID;
+                            task = new BackgroundTask(context);
+                            task.execute();
+
+//                            Intent i = new Intent(view.getContext(), ChangeThreshold.class);
+//
+//                            i.putExtra("patientThreshold", m_Text[0]);
+//                            i.putExtra("patientId", patientDoctors.get(position).patientID);
+//                            view.getContext().startActivity(i);
 
                         }
                     });
@@ -109,9 +124,58 @@ public class RecyclerAdapterDoctor extends RecyclerView.Adapter<RecyclerAdapterD
 
     }
 
+
     @Override
     public int getItemCount() {
         return patientDoctors.size();
+    }
+
+    class BackgroundTask extends AsyncTask<String, Void, Boolean> {
+
+        private Context context;
+
+        public BackgroundTask(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+
+            try {
+
+                Connection conn = SQLConnection.doInBackground();
+
+                String sql = "update healthApp.Patient set HR_Limits = " + threshold + " where Id = " + patientId;
+
+                PreparedStatement prepare = conn.prepareStatement(sql);
+
+                prepare.executeUpdate();
+                prepare.close();
+
+                return true;
+
+
+            } catch (SQLException e) {
+                Log.d(TAG, e.getMessage());
+            }
+
+            Log.d(TAG, "asynctask ouside false");
+            return true;
+
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            if (result) {
+                Log.d(TAG, "Good Job");
+                //Toast.makeText(context, "Update Threshold to " + threshold + " for patient " + patientId, Toast.LENGTH_SHORT).show();
+                notifyDataSetChanged();
+                //retrieveMessage("button");
+            } else
+                Log.d(TAG, "Good Effort");
+        }
+
     }
 
 }
