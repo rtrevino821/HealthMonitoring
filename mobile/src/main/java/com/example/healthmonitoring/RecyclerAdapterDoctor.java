a7fb6a9908b2c4f2929c1daa94f6e57b714d9454
mpetitfrere <rentenkz@gmail.com>
2016-11-28 10:16:59 -0500

    commit

--- mobile/src/main/java/com/example/healthmonitoring/RecyclerAdapterDoctor.java
+++ mobile/src/main/java/com/example/healthmonitoring/RecyclerAdapterDoctor.java
@@ -1,24 +1,19 @@
 package com.example.healthmonitoring;
 
+import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.os.AsyncTask;
-import android.support.v7.app.AlertDialog;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.text.InputType;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.List;
-
-import static com.google.android.gms.plus.PlusOneDummyView.TAG;
 
 /**
  * Created by rtrev on 10/23/2016.
@@ -26,16 +21,12 @@ import static com.google.android.gms.plus.PlusOneDummyView.TAG;
 public class RecyclerAdapterDoctor extends RecyclerView.Adapter<RecyclerAdapterDoctor.ViewHolder> {
 
     Context context;
-    String threshold;
-    String patientId;
-    private BackgroundTask task;
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
         public TextView itemPatientName;
         public TextView itemLastVisit;
         public TextView itemThreshold;
-
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -48,14 +39,13 @@ public class RecyclerAdapterDoctor extends RecyclerView.Adapter<RecyclerAdapterD
 
     List<PatientDoctor> patientDoctors;
 
-    RecyclerAdapterDoctor(List<PatientDoctor> patientDoctors){
+    RecyclerAdapterDoctor(List<PatientDoctor> patientDoctors) {
         this.patientDoctors = patientDoctors;
     }
 
 
     public RecyclerAdapterDoctor(Context context) {
     }
-
 
 
     @Override
@@ -75,107 +65,54 @@ public class RecyclerAdapterDoctor extends RecyclerView.Adapter<RecyclerAdapterD
             @Override
             public void onClick(final View view) {
 
-                    final String[] m_Text = {""};
-                    AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
-                    builder.setTitle("Please enter new Threshold");
-                    //(R.layout.activity_change_threshold);
-                    final EditText input = new EditText(view.getContext());
-                    input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_CLASS_TEXT);
-                    builder.setView(input);
+                final String[] m_Text = {""};
 
-                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            m_Text[0] = input.getText().toString();
-                            Log.d("TAG", m_Text[0]);
+                final Dialog dialog = new Dialog(view.getContext());
+                dialog.setContentView(R.layout.threshold_dialog);
 
-                            threshold = m_Text[0];;
-                            patientId = patientDoctors.get(position).patientID;
-                            task = new BackgroundTask(context);
-                            task.execute();
+                final EditText input = new EditText(view.getContext());
+                input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_CLASS_TEXT);
 
-//                            Intent i = new Intent(view.getContext(), ChangeThreshold.class);
-//
-//                            i.putExtra("patientThreshold", m_Text[0]);
-//                            i.putExtra("patientId", patientDoctors.get(position).patientID);
-//                            view.getContext().startActivity(i);
+                // set the custom dialog components - text, image and button
+                TextView text = (TextView) dialog.findViewById(R.id.text);
+                text.setText("Adjust Threshold");
 
-                        }
-                    });
-                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.cancel();
-                        }
-                    });
+                final NumberPicker np = (NumberPicker) dialog.findViewById(R.id.numberPicker);
+                np.setMaxValue(200);
+                np.setMinValue(50);
 
-                    builder.show();
+                Button ok = (Button) dialog.findViewById(R.id.dialogButtonOK);
+                ok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        m_Text[0] = String.valueOf(np.getValue());
 
+                        Intent i = new Intent(view.getContext(), ChangeThreshold.class);
+                        i.putExtra("patientThreshold", m_Text[0]);
+                        i.putExtra("patientId", patientDoctors.get(position).patientID);
+                        view.getContext().startActivity(i);
+                        dialog.dismiss();
 
+                    }
+                });
 
-                //Intent i = new Intent(view.getContext(), ChangeThreshold.class);
-               // i.putExtra("patientThreshold", patientDoctors.get(position).threshold);
+                Button cancel = (Button) dialog.findViewById(R.id.dialogButtonCancel);
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
 
-                //view.getContext().startActivity(i);
+                dialog.show();
             }
-            //   Toast.makeText(view.getContext(), "Recycle Click" + position, Toast.LENGTH_SHORT).show();
-
         });
 
     }
 
-
     @Override
     public int getItemCount() {
         return patientDoctors.size();
-    }
-
-    class BackgroundTask extends AsyncTask<String, Void, Boolean> {
-
-        private Context context;
-
-        public BackgroundTask(Context context) {
-            this.context = context;
-        }
-
-        @Override
-        protected Boolean doInBackground(String... params) {
-
-
-            try {
-
-                Connection conn = SQLConnection.doInBackground();
-
-                String sql = "update healthApp.Patient set HR_Limits = " + threshold + " where Id = " + patientId;
-
-                PreparedStatement prepare = conn.prepareStatement(sql);
-
-                prepare.executeUpdate();
-                prepare.close();
-
-                return true;
-
-
-            } catch (SQLException e) {
-                Log.d(TAG, e.getMessage());
-            }
-
-            Log.d(TAG, "asynctask ouside false");
-            return true;
-
-        }
-
-        @Override
-        protected void onPostExecute(Boolean result) {
-            if (result) {
-                Log.d(TAG, "Good Job");
-                //Toast.makeText(context, "Update Threshold to " + threshold + " for patient " + patientId, Toast.LENGTH_SHORT).show();
-                notifyDataSetChanged();
-                //retrieveMessage("button");
-            } else
-                Log.d(TAG, "Good Effort");
-        }
-
     }
 
 }
